5faa3d754b4bafe5c44d10c2c1927a92e5666815
thanhphat113 <thanhphat9523@gmail.com>
2024-10-02 13:30:13 +0700

    ChangeBase

---
+++ Frontend/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000, // Đặt port mà bạn muốn, ví dụ 8080
+  },
+})
