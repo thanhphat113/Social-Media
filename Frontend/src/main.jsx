fba532794b1bc4d275fde3c5ea4da60ae1080601
thanhphat113 <thanhphat9523@gmail.com>
2024-10-02 14:51:19 +0700

    add format

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import GlobalStyles from "./components/GlobalStyles/index.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <GlobalStyles>
+            <App />
+        </GlobalStyles>
+    </StrictMode>
+);
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Đặt port mà bạn muốn, ví dụ 8080
+    port: 3000,
   },
 })
