3f3ba609cb797aafe2d3ae44cdf6917b2acf5ed5
NgoHoangMinhTri <ngohoangminhtri0512@gmail.com>
2024-10-14 19:27:32 +0700

    14/10 19h27 btnEditAnhBia

--- Frontend/src/pages/Profile/Profile.jsx
+++ Frontend/src/pages/Profile/Profile.jsx
@@ -15,7 +15,12 @@ function Profile() {
         <div className={styles.profileWrapper}>
             <div className={styles.profileContainerLeft}>
                 <div className={styles.profileContainerLeftFirst}>
-                    <BackgroundImage />
+                    <div className={styles.containerBackground}>
+                        <BackgroundImage />
+                        <button className={styles.buttonBackground}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-pencil"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4"></path><path d="M13.5 6.5l4 4"></path></svg>
+                        </button>
+                    </div>
                     <div className={styles.profileContainerLeftFirst_2}>
                         <ProfileLeftMainTop />
                         <ProfileLeftMainTop_2 />
