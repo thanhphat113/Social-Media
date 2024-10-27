f3f8bcd9917022c4e6db56941c677d0146c5aff1
thanhphat113 <thanhphat9523@gmail.com>
2024-10-28 00:25:35 +0700

    sửa sơ css

--- Frontend/src/pages/Home/components/MainContent/index.jsx
+++ Frontend/src/pages/Home/components/MainContent/index.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { MdPhotoLibrary } from 'react-icons/md';
 =======
 <<<<<<< HEAD:Frontend/src/pages/Home/components/MainContent/index.jsx
+<<<<<<< HEAD:Frontend/src/pages/Home/components/MainContent/index.jsx
 import  { useState } from 'react';
 import {  MdPhotoLibrary } from 'react-icons/md';
 >>>>>>> 6198759 (Sidebar tạo nhóm, trang tạo nhóm, và post)
@@ -15,9 +16,20 @@ import { FaSmile, FaImage, FaMapMarkerAlt } from 'react-icons/fa';
 import { useDropzone } from 'react-dropzone';
 import styles from './MainContent.module.scss';
 import Post from '../Post';
+=======
+import React, { useState } from "react";
+import { MdOutlineVideoCall, MdPhotoLibrary } from "react-icons/md";
+import { FaSmile, FaImage, FaMapMarkerAlt } from "react-icons/fa";
+import { useDropzone } from "react-dropzone";
+import styles from "Frontend/src/pages/Home/components/MainContent/MainContent.module.scss";
+import Post from "../Post/Post";
+>>>>>>> 957ef15 (sửa sơ css):Frontend/src/pages/Home/components/MainContent/MainContent.jsx
 
 function MainContent() {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 870dc3d (sửa sơ css)
     const [comments, setComments] = useState([]);
     const [currentComment, setCurrentComment] = useState("");
     const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -28,6 +40,7 @@ function MainContent() {
     const [currentLike, setCurrentLike] = useState({
         emoji: null,
         label: "Like",
+<<<<<<< HEAD
 =======
   const [comments, setComments] = useState([]);
   const [currentComment, setCurrentComment] = useState('');
@@ -38,47 +51,52 @@ function MainContent() {
   const [posts, setPosts] = useState([]);
   const [currentLike, setCurrentLike] = useState({ emoji: null, label: 'Like' });
   const [files, setFiles] = useState([]); // State để lưu trữ nhiều file đã chọn
+=======
+    });
+    const [files, setFiles] = useState([]); // State để lưu trữ nhiều file đã chọn
+>>>>>>> 870dc3d (sửa sơ css)
 
-  const handleLikeChange = (emoji, label) => {
-    setCurrentLike({ emoji, label });
-    setIsEmojiMenuVisible(false);
-  };
+    const handleLikeChange = (emoji, label) => {
+        setCurrentLike({ emoji, label });
+        setIsEmojiMenuVisible(false);
+    };
 
-  const handleMouseEnter = () => {
-    setHoveringLike(true);
-    setIsEmojiMenuVisible(true);
-  };
+    const handleMouseEnter = () => {
+        setHoveringLike(true);
+        setIsEmojiMenuVisible(true);
+    };
 
-  const handleMouseLeave = () => {
-    if (!isEmojiMenuVisible) {
-      setHoveringLike(false);
-    }
-  };
+    const handleMouseLeave = () => {
+        if (!isEmojiMenuVisible) {
+            setHoveringLike(false);
+        }
+    };
 
-  const handleAddComment = () => {
-    if (currentComment.trim() !== '') {
-      setComments([...comments, currentComment]);
-      setCurrentComment('');
-    }
-  };
+    const handleAddComment = () => {
+        if (currentComment.trim() !== "") {
+            setComments([...comments, currentComment]);
+            setCurrentComment("");
+        }
+    };
 
-  const handlePostSubmit = () => {
-    if (postContent.trim() !== '') {
-      const newPost = {
-        images: files.map(file => file.preview), // Thay đổi thành mảng các ảnh
-        title: "Anime",
-        userName: "Nguyễn Tiến",
-        content: postContent,
-        time: "Mới đây"
-      };
+    const handlePostSubmit = () => {
+        if (postContent.trim() !== "") {
+            const newPost = {
+                images: files.map((file) => file.preview), // Thay đổi thành mảng các ảnh
+                title: "Anime",
+                userName: "Nguyễn Tiến",
+                content: postContent,
+                time: "Mới đây",
+            };
 
-      setPosts([newPost, ...posts]);
-      setPostContent('');
-      setFiles([]); // Reset files
-      setIsPopupOpen(false);
-    }
-  };
+            setPosts([newPost, ...posts]);
+            setPostContent("");
+            setFiles([]); // Reset files
+            setIsPopupOpen(false);
+        }
+    };
 
+<<<<<<< HEAD:Frontend/src/pages/Home/components/MainContent/index.jsx
   const { getRootProps, getInputProps } = useDropzone({
     accept: 'image/*',
     onDrop: acceptedFiles => {
@@ -86,6 +104,7 @@ function MainContent() {
         console.log(file.type); // Kiểm tra kiểu MIME của file
 >>>>>>> 32c7979 (Thay cách thức repository được gọi)
     });
+<<<<<<< HEAD
     const [files, setFiles] = useState([]); // State để lưu trữ nhiều file đã chọn
     const [isSharePopupOpen, setIsSharePopupOpen] = useState(false);
     const [visibility, setVisibility] = useState("Công khai");
@@ -137,6 +156,33 @@ function MainContent() {
         }
     };
 =======
+=======
+      setFiles(acceptedFiles.map(file => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })));
+    }
+  });
+=======
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: "image/*,video/*",
+        onDrop: (acceptedFiles) => {
+            setFiles(
+                acceptedFiles.map((file) =>
+                    Object.assign(file, {
+                        preview: URL.createObjectURL(file),
+                    })
+                )
+            );
+        },
+    });
+>>>>>>> 957ef15 (sửa sơ css):Frontend/src/pages/Home/components/MainContent/MainContent.jsx
+
+    const togglePopup = () => {
+        setIsPopupOpen(!isPopupOpen);
+    };
+
+<<<<<<< HEAD:Frontend/src/pages/Home/components/MainContent/index.jsx
+>>>>>>> 870dc3d (sửa sơ css)
   return (
     <main className={styles.content}>
       <div className={styles.postContainer}>
@@ -397,7 +443,30 @@ function MainContent() {
           </div>
         </>
       )}
+=======
+    return (
+        <main className={styles.content}>
+            <div className={styles.postContainer}>
+                <input
+                    type="text"
+                    placeholder="Tiến ơi, bạn đang nghĩ gì thế?"
+                    className={styles.inputField}
+                    onFocus={togglePopup}
+                />
+                <div className={styles.line}></div>
+                <div className={styles.actionButtons}>
+                    <button
+                        className={styles.photoButton}
+                        onClick={togglePopup}
+                    >
+                        <MdPhotoLibrary className={styles.iconGreen} />
+                        Ảnh/video
+                    </button>
+                </div>
+            </div>
+>>>>>>> 957ef15 (sửa sơ css):Frontend/src/pages/Home/components/MainContent/MainContent.jsx
 
+<<<<<<< HEAD
       {posts.map((post, index) => (
         <Post
           key={index}
@@ -419,6 +488,87 @@ function MainContent() {
     </main>
   );
 >>>>>>> 32c7979 (Thay cách thức repository được gọi)
+=======
+            {isPopupOpen && (
+                <>
+                    <div
+                        className={styles.popupOverlay}
+                        onClick={togglePopup}
+                    ></div>
+                    <div className={styles.popup}>
+                        <div className={styles.popupHeader}>
+                            <h2 className={styles.popupTitle}>Tạo bài viết</h2>
+                            <button
+                                className={styles.closeButton}
+                                onClick={togglePopup}
+                            >
+                                X
+                            </button>
+                        </div>
+                        <div className={styles.userInfo}>
+                            <img
+                                src="profile.jpg"
+                                alt="Profile"
+                                className={styles.profileImage}
+                            />
+                            <div className={styles.userName}>
+                                <p className={styles.userNameText}>
+                                    Nguyễn Tiến
+                                </p>
+                                <button className={styles.publicButton}>
+                                    Công khai
+                                </button>
+                            </div>
+                        </div>
+                        <textarea
+                            className={styles.textarea}
+                            placeholder="Tiến ơi, bạn đang nghĩ gì thế?"
+                            value={postContent}
+                            onChange={(e) => setPostContent(e.target.value)}
+                        />
+                        <div {...getRootProps({ className: styles.dropzone })}>
+                            <input {...getInputProps()} />
+                            <p>Thêm ảnh/video hoặc kéo và thả</p>
+                        </div>
+                        <div className={styles.extraOptions}>
+                            <p>Thêm vào bài viết của bạn</p>
+                            <div className={styles.iconOptions}>
+                                <FaImage className={styles.iconGreen} />
+                                <FaSmile className={styles.iconYellow} />
+                                <FaMapMarkerAlt className={styles.iconRed} />
+                                <span className={styles.iconPurple}>GIF</span>
+                            </div>
+                        </div>
+                        <button
+                            className={styles.continueButton}
+                            onClick={handlePostSubmit}
+                        >
+                            Đăng
+                        </button>
+                    </div>
+                </>
+            )}
+
+            {posts.map((post, index) => (
+                <Post
+                    key={index}
+                    post={post}
+                    currentLike={currentLike}
+                    setCurrentLike={setCurrentLike}
+                    handleLikeChange={handleLikeChange}
+                    hoveringLike={hoveringLike}
+                    handleMouseEnter={handleMouseEnter}
+                    handleMouseLeave={handleMouseLeave}
+                    isEmojiMenuVisible={isEmojiMenuVisible}
+                    comments={comments}
+                    currentComment={currentComment}
+                    handleAddComment={handleAddComment}
+                    setCurrentComment={setCurrentComment}
+                />
+            ))}
+        </main>
+    );
+>>>>>>> 870dc3d (sửa sơ css)
 }
 
 export default MainContent;
